1f284df3ff47957c04bb0f1da8be6ec28ae6a0db
dustinvo17 <datvo.vtd@gmail.com>
2020-09-25 16:39:03 -0500

    adj graph traversal

---
+++ graph_traversal/bfs_graph_traversal.py
@@ -0,0 +1,30 @@
+graph_adj_list = {
+    "A": ["B", "C"],
+    "B": ["A", "D"],
+    "C": ["A", "E"],
+    "D": ["B", "E", "F"],
+    "E": ["C", "D", "F"],
+    "F": ["D", "E"]
+}
+# //          A
+# //        /   \
+# //       B     C
+# //       |     |
+# //       D --- E
+# //        \   /
+# //          F
+
+def bfs_graph(v):
+  queue = [v]
+  result = []
+  visited = {v:True}
+  while len(queue) > 0:
+    out_vertext = queue.pop(0)
+    result.append(out_vertext)
+    for v_t in graph_adj_list[out_vertext]:
+      if v_t not in visited.keys():
+        visited[v_t] = True
+        queue.append(v_t)
+  return result
+
+assert bfs_graph('A') == ["A","B","C","D","E","F"]
---
+++ graph_traversal/dfs_graph_traversal.py
@@ -0,0 +1,67 @@
+graph_adj_list = {
+    "A": ["B", "C"],
+    "B": ["A", "D"],
+    "C": ["A", "E"],
+    "D": ["B", "E", "F"],
+    "E": ["C", "D", "F"],
+    "F": ["D", "E"]
+}
+# //          A
+# //        /   \
+# //       B     C
+# //       |     |
+# //       D --- E
+# //        \   /
+# //          F
+
+
+
+
+def graph_dfs_recursive(v, result=[], visited={}):
+    if not v:
+      return NotImplemented
+    result.append(v)
+    visited[v] = True
+    print(result, visited)
+    for v_t in graph_adj_list[v]:
+        if v_t not in visited.keys():
+            graph_dfs_recursive(v_t, result, visited)
+    return result
+
+
+assert graph_dfs_recursive("A") == ["A", "B", "D", "E", "C", "F"]
+
+
+# "A": ["B", "C"],
+# "B": ["A", "D"],
+# "C": ["A", "E"],
+# "D": ["B", "E", "F"],
+# "E": ["C", "D", "F"],
+# "F": ["D", "E"]
+
+# //          A
+# //        /   \
+# //       B     C
+# //       |     |
+# //       D --- E
+# //        \   /
+# //          F
+
+def graph_dfs_iterative(v):
+
+  stack = [v]
+  result = []
+  visited = {v:True}
+  while len(stack) > 0:   
+    out_vertext = stack.pop() 
+    result.append(out_vertext)   
+    for v_t in graph_adj_list[out_vertext]:
+      if v_t not in visited.keys():
+        stack.append(v_t)
+        visited[v_t] = True
+  print(result)
+  return result
+
+
+
+assert graph_dfs_iterative("A") == ["A","C","E","F","D","B"]
